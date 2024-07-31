338d44e031131816a25828dbe2fce84115c42d1c
blackcat5328 <167646747+blackcat5328@users.noreply.github.com>
2024-07-31 16:58:21 +0800

    Update whack_a_mole.js

--- src/whack_a_mole.js
+++ src/whack_a_mole.js
@@ -217,7 +217,7 @@ window.initGame = (React, assetsUrl) => {
         'p',
         null,
         winner === 0
-          ? `Current player: ${currentPlayer === 1 ? 'Player 1' : 'Player 2'} (${timer} seconds remaining).`
+          ? `Current player: ${currentPlayer === 1 ? 'BLACK' : 'WHITE'} (${timer} seconds remaining).`
           : `Player ${winner} wins!`
       ),
       React.createElement(
